55bd082b557ea9712c10407766380d00568a64ef
Giang Nguyen <thuygiang152@gmail.com>
2019-12-01 00:58:08 +0200

    Styles fixed: changing style of movie poster

--- movie-app/hooked/src/components/Movie.js
+++ movie-app/hooked/src/components/Movie.js
@@ -9,12 +9,8 @@ const Movie = ({ movie }) => {
   return (
     <div className="movie">
       <h2>{movie.Title}</h2>
-      <div>
-        <img
-          width="200"
-          alt={`The movie titled: ${movie.Title}`}
-          src={poster}
-        />
+      <div style={{ objectFit: "fill" }}>
+        <img alt={`The movie titled: ${movie.Title}`} src={poster} />
       </div>
       <p>{movie.Year}</p>
     </div>
