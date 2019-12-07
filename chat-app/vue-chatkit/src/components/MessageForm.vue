97c29a61d92cf6fd5a2119de33aedab0727424ae
Giang Nguyen <thuygiang152@gmail.com>
2019-12-07 02:16:38 +0200

    Handle message sending and emitting of typing events

--- chat-app/vue-chatkit/src/components/MessageForm.vue
+++ chat-app/vue-chatkit/src/components/MessageForm.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="message-form ld-over">
     <small class="text-muted">@{{ user.username }}</small>
-    <b-form
-      @submit.prevent="onSubmit"
-      class="ld-over"
-      v-bind:class="{ running, sending }"
-    >
+    <b-form @submit.prevent="onSubmit" class="ld-over" v-bind:class="{ running, sending }">
       <div class="ld ld-ring ld-spin"></div>
       <b-alert variant="danger" :show="hasError">{{ error }}</b-alert>
       <b-form-group>
@@ -13,23 +9,22 @@
           id="message-input"
           type="text"
           v-model="message"
+          @input="isTyping"
           placeholder="Enter Message"
           autocomplete="off"
           required
-        >
-        </b-form-input>
+        ></b-form-input>
       </b-form-group>
       <div class="clearfix">
-        <b-button type="submit" variant="primary" class="float-right">
-          Send
-        </b-button>
+        <b-button type="submit" variant="primary" class="float-right">Send</b-button>
       </div>
     </b-form>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
+import { isTyping } from "../chatkit.js";
 
 export default {
   name: "message-form",
@@ -41,6 +36,19 @@ export default {
   computed: {
     ...mapState(["user", "sending", "error", "activeRoom"]),
     ...mapGetters(["hasError"])
+  },
+  methods: {
+    ...mapActions(["sendMessage"]),
+
+    async onSubmit() {
+      const result = await this.sendMessage(this.message);
+      if (result) {
+        this.message = "";
+      }
+    },
+    async isTyping() {
+      await isTyping(this.activeRoom.id);
+    }
   }
 };
 </script>
