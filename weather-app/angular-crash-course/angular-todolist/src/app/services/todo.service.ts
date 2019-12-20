a15400bd21b267d09e25846e057eced59fa0bd66
Giang Nguyen <thuygiang152@gmail.com>
2019-12-20 23:52:26 +0200

    Mark a todo as done in the server

--- weather-app/angular-crash-course/angular-todolist/src/app/services/todo.service.ts
+++ weather-app/angular-crash-course/angular-todolist/src/app/services/todo.service.ts
@@ -4,15 +4,26 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Todo } from "../models/Todo";
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+};
+
 @Injectable({
   providedIn: "root"
 })
 export class TodoService {
   todosUrl: string = "https://jsonplaceholder.typicode.com/todos";
-
+  todosLimit = "?_limit=10";
   constructor(private http: HttpClient) {}
 
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosUrl);
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
