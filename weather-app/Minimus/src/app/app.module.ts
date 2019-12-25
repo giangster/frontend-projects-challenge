321570047c12d371f1843e0a3990c84c47336bdf
Giang Nguyen <thuygiang152@gmail.com>
2019-12-25 23:51:55 +0200

    Weather card added

--- weather-app/Minimus/src/app/app.module.ts
+++ weather-app/Minimus/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AddCardComponent } from './components/add-card/add-card.component';
+import { DetailsComponent } from './components/details/details.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AddCardComponent,
+    DetailsComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
