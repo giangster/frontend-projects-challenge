6e560e2a846114fa59387254b8d8ec388e331085
Giang Nguyen <thuygiang152@gmail.com>
2019-12-29 23:53:01 +0200

    Function addTodo

--- todo-app/svelte-todo/src/Todos.svelte
+++ todo-app/svelte-todo/src/Todos.svelte
@@ -22,29 +22,43 @@
       completed: false
     }
   ];
+
+  function addTodo(event) {
+    if (event.key === 'Enter') {
+        todos = [...todos, {
+            id: nextId,
+            completed: false,
+            title: newTodoTitle
+        }];
+        nextId = nextId + 1;
+        newTodoTitle = '';
+    }
+}
 </script>
 
-<div class="container">
-        <a href="https://codingthesmartway.com" target="_blank"><img src={'/img/CTSWLogo.png'} alt="svelte logo" class="logo"></a>
-        <h2>Svelte Todo App</h2>
-        <input type="text" class="todo-input" placeholder="Insert todo item ..." bind:value={newTodoTitle} on:keydown={addTodo}>
-        {#each filteredTodos as todo}
-            <div class="todo-item">
-                <TodoItem {...todo} on:deleteTodo={handleDeleteTodo} on:toggleComplete={handleToggleComplete} />
-            </div>
-        {/each}
-        <div class="inner-container">
-            <div><label><input class="inner-container-input" type="checkbox" on:change={checkAllTodos}>Check All</label></div>
-            <div>{todosRemaining} items left</div>
+<main>
+  <div class="container">
+    <a href="https://codingthesmartway.com" target="_blank"><img src={'/img/CTSWLogo.png'} alt="svelte logo" class="logo"></a>
+    <h2>Svelte Todo App</h2>
+    <input type="text" class="todo-input" placeholder="Insert todo item ..." bind:value={newTodoTitle} on:keydown={addTodo}>
+    {#each filteredTodos as todo}
+        <div class="todo-item">
+            <TodoItem {...todo} on:deleteTodo={handleDeleteTodo} on:toggleComplete={handleToggleComplete} />
         </div>
-        <div class="inner-container">
-            <div>
-                <button on:click={() => updateFilter('all')} class:active="{currentFilter === 'all'}">All</button>
-                <button on:click={() => updateFilter('active')} class:active="{currentFilter === 'active'}">Active</button>
-                <button on:click={() => updateFilter('completed')} class:active="{currentFilter === 'completed'}">Completed</button>
-            </div>
-            <div>
-                <button on:click={clearCompleted}>Clear Completed</button>
-            </div>
+    {/each}
+    <div class="inner-container">
+        <div><label><input class="inner-container-input" type="checkbox" on:change={checkAllTodos}>Check All</label></div>
+        <div>{todosRemaining} items left</div>
+    </div>
+    <div class="inner-container">
+        <div>
+            <button on:click={() => updateFilter('all')} class:active="{currentFilter === 'all'}">All</button>
+            <button on:click={() => updateFilter('active')} class:active="{currentFilter === 'active'}">Active</button>
+            <button on:click={() => updateFilter('completed')} class:active="{currentFilter === 'completed'}">Completed</button>
+        </div>
+        <div>
+            <button on:click={clearCompleted}>Clear Completed</button>
         </div>
     </div>
+</div>
+</main>
